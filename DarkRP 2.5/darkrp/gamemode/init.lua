baae47e402feda51015d8335d245072210695b8c
wolfh4ley <drew.riemer@gmail.com>
2013-12-25 12:19:44 -0600

    Fixed doors and jail positions not saving.

--- DarkRP 2.5/darkrp/gamemode/init.lua
+++ DarkRP 2.5/darkrp/gamemode/init.lua
@@ -3,66 +3,38 @@ GM.Name = "DarkRP"
 GM.Author = "By Rickster, Updated: Pcwizdan, Sibre, philxyz, [GNC] Matt, Chrome Bolt, FPtje Falco, Eusion, Drakehawke"
 
 DeriveGamemode("sandbox")
+local function LoadModules()
+        local root = GM.FolderName.."/gamemode/modules/"
 
-AddCSLuaFile("libraries/interfaceloader.lua")
-AddCSLuaFile("libraries/modificationloader.lua")
-AddCSLuaFile("libraries/fn.lua")
+        local _, folders = file.Find(root.."*", "LUA")
 
-AddCSLuaFile("config/config.lua")
-AddCSLuaFile("config/addentities.lua")
-AddCSLuaFile("config/jobrelated.lua")
-AddCSLuaFile("config/ammotypes.lua")
+        for _, folder in SortedPairs(folders, true) do
+                if DarkRP.disabledDefaults["modules"][folder] then continue end
 
-AddCSLuaFile("cl_init.lua")
+                for _, File in SortedPairs(file.Find(root .. folder .."/sh_*.lua", "LUA"), true) do
+                        if File == "sh_interface.lua" then continue end
+                        include(root.. folder .. "/" ..File)
+                end
+                for _, File in SortedPairs(file.Find(root .. folder .."/cl_*.lua", "LUA"), true) do
+                        if File == "cl_interface.lua" then continue end
+                        include(root.. folder .. "/" ..File)
+                end
+        end
+end
+
+GM.Config = {} -- config table
+GM.NoLicense = GM.NoLicense or {}
+
 AddCSLuaFile("sh_config.lua")
 include("sh_config.lua")
 
-GM.Config = GM.Config or {}
-GM.NoLicense = GM.NoLicense or {}
-
-include("libraries/interfaceloader.lua")
-
-include("config/_MySQL.lua")
 include("config/config.lua")
-include("config/licenseweapons.lua")
+include("libraries/fn.lua")
+include("libraries/interfaceloader.lua")
+include("libraries/disjointset.lua")
 
 include("libraries/modificationloader.lua")
-include("libraries/fn.lua")
-include("libraries/database.lua")
-
-/*---------------------------------------------------------------------------
-Loading modules
----------------------------------------------------------------------------*/
-local fol = GM.FolderName.."/gamemode/modules/"
-local files, folders = file.Find(fol .. "*", "LUA")
-for k,v in pairs(files) do
-	if DarkRP.disabledDefaults["modules"][k] then continue end
-
-	include(fol .. v)
-end
-
-for _, folder in SortedPairs(folders, true) do
-	if folder == "." or folder == ".." or DarkRP.disabledDefaults["modules"][folder] then continue end
-
-	for _, File in SortedPairs(file.Find(fol .. folder .."/sh_*.lua", "LUA"), true) do
-		AddCSLuaFile(fol..folder .. "/" ..File)
-
-		if File == "sh_interface.lua" then continue end
-		include(fol.. folder .. "/" ..File)
-	end
-
-	for _, File in SortedPairs(file.Find(fol .. folder .."/sv_*.lua", "LUA"), true) do
-		if File == "sv_interface.lua" then continue end
-		include(fol.. folder .. "/" ..File)
-	end
-
-	for _, File in SortedPairs(file.Find(fol .. folder .."/cl_*.lua", "LUA"), true) do
-		if File == "cl_interface.lua" then continue end
-		AddCSLuaFile(fol.. folder .. "/" ..File)
-	end
-end
-
-MySQLite.initialize()
+LoadModules()
 
 DarkRP.DARKRP_LOADING = true
 include("config/jobrelated.lua")
